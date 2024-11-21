46408bcd089214b6941f72831a96dff92020d0dd
Zhen Lu <zhenlu@lightspark.com>
2024-11-21 11:02:13 -0800

    Add a testcase to run generate deposit address (#46)
    Add logs and close the channel after wg is done.

--- spark/so/grpc/spark_internal_server.go
+++ spark/so/grpc/spark_internal_server.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"log"
 
 	"github.com/google/uuid"
 	pb "github.com/lightsparkdev/spark-go/proto"
@@ -20,18 +21,23 @@ func NewSparkInternalServer(config *so.Config) *SparkInternalServer {
 }
 
 func (s *SparkInternalServer) MarkKeysharesAsUsed(ctx context.Context, req *pb.MarkKeysharesAsUsedRequest) (*emptypb.Empty, error) {
+	log.Printf("Marking keyshares as used: %v", req.KeyshareId)
 	ids := make([]uuid.UUID, len(req.KeyshareId))
 	for i, id := range req.KeyshareId {
 		uuid, err := uuid.Parse(id)
 		if err != nil {
+			log.Printf("Failed to parse keyshare ID: %v", err)
 			return nil, err
 		}
 		ids[i] = uuid
 	}
 	err := ent_utils.MarkSigningKeysharesAsUsed(ctx, s.config, ids)
 	if err != nil {
+		log.Printf("Failed to mark keyshares as used: %v", err)
 		return nil, err
 	}
+
+	log.Printf("Marked keyshares as used")
 
 	return &emptypb.Empty{}, nil
 }
--- spark/so/grpc/spark_server.go
+++ spark/so/grpc/spark_server.go
@@ -2,7 +2,9 @@ package grpc
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
+	"log"
 	"sync"
 
 	"github.com/google/uuid"
@@ -22,12 +24,14 @@ func NewSparkServer(config *so.Config) *SparkServer {
 }
 
 func (s *SparkServer) GenerateDepositAddress(ctx context.Context, req *pb.GenerateDepositAddressRequest) (*pb.GenerateDepositAddressResponse, error) {
+	log.Printf("Generating deposit address for public key: %s", hex.EncodeToString(req.PublicKey))
 	keyshares, err := ent_utils.GetUnusedSigningKeyshares(ctx, s.config, 1)
 	if err != nil {
 		return nil, err
 	}
 
 	if len(keyshares) == 0 {
+		log.Printf("No keyshares available")
 		return nil, fmt.Errorf("no keyshares available")
 	}
 
@@ -35,6 +39,7 @@ func (s *SparkServer) GenerateDepositAddress(ctx context.Context, req *pb.Genera
 
 	err = ent_utils.MarkSigningKeysharesAsUsed(ctx, s.config, []uuid.UUID{keyshare.ID})
 	if err != nil {
+		log.Printf("Failed to mark keyshare as used: %v", err)
 		return nil, err
 	}
 
@@ -50,21 +55,25 @@ func (s *SparkServer) GenerateDepositAddress(ctx context.Context, req *pb.Genera
 		wg.Add(1)
 
 		go func(operator *so.SigningOperator) {
+			defer wg.Done()
+			log.Printf("Connecting to operator: %s", operator.Address)
 			conn, err := common.NewGRPCConnection(operator.Address)
 			if err != nil {
 				results <- err
+				log.Printf("Failed to connect to operator: %v", err)
 				return
 			}
 
 			client := pb.NewSparkInternalServiceClient(conn)
 			_, err = client.MarkKeysharesAsUsed(ctx, &pb.MarkKeysharesAsUsedRequest{KeyshareId: []string{keyshare.ID.String()}})
 			results <- err
-
-			wg.Done()
+			log.Printf("Marked keyshare as used on operator: %s", operator.Address)
 		}(operator)
 	}
 
+	log.Printf("Waiting for all operators to mark keyshare as used")
 	wg.Wait()
+	close(results)
 
 	var errors []error
 	for err := range results {
@@ -74,20 +83,24 @@ func (s *SparkServer) GenerateDepositAddress(ctx context.Context, req *pb.Genera
 	}
 
 	if len(errors) > 0 {
+		log.Printf("Failed to mark keyshares as used on all operators: %v", errors)
 		return nil, fmt.Errorf("failed to mark keyshares as used on all operators: %v", errors)
 	}
 
 	combinedPublicKey, err := common.AddPublicKeys(keyshare.PublicKey, req.PublicKey)
 	if err != nil {
+		log.Printf("Failed to add public keys: %v", err)
 		return nil, err
 	}
 
 	depositAddress, err := common.P2TRAddressFromPublicKey(combinedPublicKey, s.config.Network)
 	if err != nil {
+		log.Printf("Failed to generate deposit address: %v", err)
 		return nil, err
 	}
 
 	// TODO: we need to store the deposit address along with the keyshare ID.
 
+	log.Printf("Generated deposit address: %s", depositAddress)
 	return &pb.GenerateDepositAddressResponse{Address: *depositAddress}, nil
 }
