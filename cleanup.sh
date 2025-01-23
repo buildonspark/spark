a90a60a672c913fe45c1923319e4e3f4c5cd8fbb
Zhen Lu <zhenlu@lightspark.com>
2025-01-22 18:12:48 -0800

    cleanup.sh should also wipe db (#185)

--- cleanup.sh
+++ cleanup.sh
@@ -2,4 +2,22 @@
 
 tmux kill-session -t frost-signers
 tmux kill-session -t operators
+
+# Terminate all relevant connections first
+for i in $(seq 0 4); do
+    db="operator_$i"
+    psql postgres -c "
+    SELECT pg_terminate_backend(pid) 
+    FROM pg_stat_activity 
+    WHERE datname = '$db' 
+    AND pid <> pg_backend_pid();" > /dev/null 2>&1
+done
+
+# Drop and recreate
+for i in $(seq 0 4); do
+    db="operator_$i"
+    echo "Resetting $db..."
+    dropdb --if-exists "$db" > /dev/null 2>&1
+    createdb "$db" > /dev/null 2>&1
+done
 rm -rf _data
