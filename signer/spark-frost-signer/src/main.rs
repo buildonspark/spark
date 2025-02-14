0882d8faf230621de8a987ccd609e3ca51b0a715
Zhen Lu <zhenlu@lightspark.com>
2025-02-14 10:53:19 -0800

    Handle SIGTERM and SIGINT (#414)

--- signer/spark-frost-signer/src/main.rs
+++ signer/spark-frost-signer/src/main.rs
@@ -1,9 +1,18 @@
-use std::fs;
+use std::{
+    fs,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
 
 use clap::{arg, Parser};
 use server::FrostServer;
 use spark_frost::proto::frost::frost_service_server;
-use tokio::net::{TcpListener, UnixListener};
+use tokio::{
+    net::{TcpListener, UnixListener},
+    sync::mpsc,
+};
 use tonic::transport::Server;
 use tracing::Level;
 
@@ -25,6 +34,32 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+
+    // Create shutdown signal
+    let (shutdown_tx, mut shutdown_rx) = mpsc::channel(1);
+    let shutdown_signal = Arc::new(AtomicBool::new(false));
+    let shutdown_signal_clone = shutdown_signal.clone();
+
+    // Spawn signal handler task
+    tokio::spawn(async move {
+        let mut sigterm =
+            tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())?;
+        let mut sigint = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt())?;
+
+        tokio::select! {
+            _ = sigterm.recv() => {
+                tracing::info!("Received SIGTERM");
+            }
+            _ = sigint.recv() => {
+                tracing::info!("Received SIGINT");
+            }
+        }
+
+        // Set shutdown signal
+        shutdown_signal_clone.store(true, Ordering::Relaxed);
+        let _ = shutdown_tx.send(()).await;
+        Ok::<_, std::io::Error>(())
+    });
 
     tracing_subscriber::fmt()
         .with_target(false)
@@ -55,7 +90,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let stream = tokio_stream::wrappers::UnixListenerStream::new(listener);
             Server::builder()
                 .add_service(frost_service_server::FrostServiceServer::new(frost_server))
-                .serve_with_incoming(stream)
+                .serve_with_incoming_shutdown(stream, async {
+                    shutdown_rx.recv().await;
+                    tracing::info!("Shutting down");
+                })
                 .await?;
         }
         _ => return Err("Invalid listen options".into()),
