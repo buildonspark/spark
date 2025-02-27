cd4d4008e3b6f57d272cc778932570125bd67259
Brian Chiang <brianc@lightspark.com>
2025-02-27 11:45:35 -0800

    this change allows the mainnet toggle to always be visible (#798)

--- sdks/js/examples/spark-demo/src/pages/login/Login.tsx
+++ sdks/js/examples/spark-demo/src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import { Network } from "@buildonspark/spark-sdk/utils";
 import { useEffect, useMemo, useState } from "react";
 import { isValidPhoneNumber } from "react-phone-number-input";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import PhoneInput from "../../components/PhoneInput";
 import VerificationCode from "../../components/VerificationCode";
@@ -21,7 +21,6 @@ export default function Login() {
   const [verificationCode, setVerificationCode] = useState<string | undefined>(
     "",
   );
-  const [searchParams] = useSearchParams();
 
   const isValidPhone = useMemo(() => {
     return phoneNumber && isValidPhoneNumber(phoneNumber);
@@ -100,47 +99,45 @@ export default function Login() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      {searchParams.get("user") === "davidmarcus" && (
-        <div className="mb-1 flex w-full flex-row items-center justify-end">
-          <div
-            className={`mr-2 text-center font-decimal text-[13px] ${
+      <div className="mb-1 flex w-full flex-row items-center justify-end">
+        <div
+          className={`mr-2 text-center font-decimal text-[13px] ${
+            initWalletNetwork === Network.MAINNET
+              ? "text-[#ffffff]"
+              : "text-[#ffffff]"
+          }`}
+        >
+          {initWalletNetwork === Network.MAINNET ? "Mainnet" : "Regtest"}
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              const newNetwork =
+                initWalletNetwork === Network.MAINNET
+                  ? Network.REGTEST
+                  : Network.MAINNET;
+              localStorage.setItem(
+                "spark_wallet_network",
+                newNetwork.toString(),
+              );
+              setInitWalletNetwork(newNetwork);
+            }}
+            className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors ${
               initWalletNetwork === Network.MAINNET
-                ? "text-[#ffffff]"
-                : "text-[#ffffff]"
+                ? "bg-white"
+                : "bg-[#696969]"
             }`}
           >
-            {initWalletNetwork === Network.MAINNET ? "Mainnet" : "Regtest"}
-          </div>
-          <div>
-            <button
-              onClick={() => {
-                const newNetwork =
-                  initWalletNetwork === Network.MAINNET
-                    ? Network.REGTEST
-                    : Network.MAINNET;
-                localStorage.setItem(
-                  "spark_wallet_network",
-                  newNetwork.toString(),
-                );
-                setInitWalletNetwork(newNetwork);
-              }}
-              className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors ${
+            <span
+              className={`inline-block h-4 w-4 transform rounded-full transition-transform ${
                 initWalletNetwork === Network.MAINNET
-                  ? "bg-white"
-                  : "bg-[#696969]"
+                  ? "translate-x-6 bg-black"
+                  : "translate-x-1 bg-white"
               }`}
-            >
-              <span
-                className={`inline-block h-4 w-4 transform rounded-full transition-transform ${
-                  initWalletNetwork === Network.MAINNET
-                    ? "translate-x-6 bg-black"
-                    : "translate-x-1 bg-white"
-                }`}
-              />
-            </button>
-          </div>
+            />
+          </button>
         </div>
-      )}
+      </div>
       <div className="font-inter mt-4 text-center text-[13px] text-[#ffffff] opacity-40">
         A Spark-enabled, self-custody
         <br />
