54e38911fbd48c671490466a8512fb5f4103e27e
Carson <cpartee6@gmail.com>
2025-03-11 13:26:59 -0700

    Fix 'Buffer is not defined' error in browser (#892)

--- sdks/js/packages/lrc20-sdk/src/bitcoin-core.ts
+++ sdks/js/packages/lrc20-sdk/src/bitcoin-core.ts
@@ -1,3 +1,4 @@
+import "./buffer";
 // import ecc from "@bitcoinerlab/secp256k1";
 import * as bitcoin from "bitcoinjs-lib";
 
@@ -6,4 +7,4 @@ import { ECPairFactory } from "ecpair";
 
 bitcoin.initEccLib(ecc);
 const ECPair = ECPairFactory(ecc);
-export { ECPair, bitcoin, ecc };
+export { bitcoin, ecc, ECPair };
