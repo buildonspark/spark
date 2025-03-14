0f0636af8d6a8d26c5b1a30983031137b3b950dd
Brian Chiang <brianc@lightspark.com>
2025-03-13 18:23:24 -0700

    this change adds stream to the lrc20-sdk and polyfills window (#941)

--- sdks/js/examples/spark-demo/config-overrides.js
+++ sdks/js/examples/spark-demo/config-overrides.js
@@ -1,6 +1,3 @@
-module.exports = function override (config, env) {
-    config.resolve.fallback = Object.assign(config.resolve.fallback || {}, {
-        stream: require.resolve('stream-browserify'),
-    })
-    return config
-}
+module.exports = function override(config, env) {
+  return config;
+};
--- sdks/js/packages/lrc20-sdk/src/buffer.ts
+++ sdks/js/packages/lrc20-sdk/src/buffer.ts
@@ -1,2 +1,6 @@
 import { Buffer } from "buffer";
 globalThis.Buffer = Buffer;
+
+if (typeof global === "undefined") {
+  (window as any).global = window;
+}
