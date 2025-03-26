6a8096210d3f5364f44c48a3df1732c6e44d90d5
Carson <cpartee6@gmail.com>
2025-03-25 17:53:19 -0700

    [spark-sdk] Update checks in crypto.ts (#1078)

--- sdks/js/packages/spark-sdk/src/utils/crypto.ts
+++ sdks/js/packages/spark-sdk/src/utils/crypto.ts
@@ -1,15 +1,18 @@
 import nodeCrypto from "crypto";
 
 export const getCrypto = (): Crypto => {
-  let cryptoImpl: any = typeof window !== "undefined" ? window.crypto :
-                        typeof global !== "undefined" ? global.crypto :
-                        nodeCrypto;
+  let cryptoImpl: any =
+    typeof window !== "undefined"
+      ? window.crypto
+      : typeof global !== "undefined" && global.crypto
+        ? global.crypto
+        : nodeCrypto;
 
   // Add randomUUID if it doesn't exist
-  if (!cryptoImpl.randomUUID && nodeCrypto.randomUUID) {
+  if (!cryptoImpl?.randomUUID && nodeCrypto.randomUUID) {
     cryptoImpl = {
       ...cryptoImpl,
-      randomUUID: nodeCrypto.randomUUID
+      randomUUID: nodeCrypto.randomUUID,
     };
   }
 
