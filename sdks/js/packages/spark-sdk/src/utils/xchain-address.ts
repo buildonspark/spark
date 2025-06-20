c9dbf0435922a97e0b3d85b8be931385966efd43
Steven Lee <steven.lee@magiceden.io>
2025-06-20 13:19:28 -0700

    removed use of bitcoinjs-lib

--- sdks/js/packages/spark-sdk/src/utils/xchain-address.ts
+++ sdks/js/packages/spark-sdk/src/utils/xchain-address.ts
@@ -1,26 +1,29 @@
-import * as bitcoin from 'bitcoinjs-lib';
 import { encodeSparkAddress, SparkAddressFormat } from '../address/address.js';
-import { ValidationError } from '../index.node.js';
+import * as btc from '@scure/btc-signer';
+import { ValidationError } from '../errors/index.js';
 
 const networkByType = {
-  bitcoin: 'MAINNET',
-  regtest: 'REGTEST',
-  testnet: 'TESTNET',
+  MAINNET: btc.NETWORK,
+  TESTNET: btc.TEST_NETWORK,
+  REGTEST: {
+    ...btc.TEST_NETWORK,
+    bech32: 'bcrt',
+  },
 } as const;
 
 export function getSparkAddressFromTaproot(taprootAddress: string): SparkAddressFormat {
-  for (const networkType of ['bitcoin', 'regtest', 'testnet'] as const) {
-    const network = bitcoin.networks[networkType];
+  for (const networkType of ['MAINNET', 'TESTNET', 'REGTEST'] as const) {
     try {
-      bitcoin.payments.p2tr({address: taprootAddress, network});
+      const result = btc.Address(networkByType[networkType]).decode(taprootAddress);
+      if (result.type === 'tr') {
+        const outputPublicKey = result.pubkey;
+        return encodeSparkAddress({
+          identityPublicKey: Buffer.concat([Buffer.from([0x02]), outputPublicKey]).toString('hex'),
+          network: networkType,
+        })
+      }
     } catch (_) {
-      continue;
     }
-    const { data: outputPublicKey } = bitcoin.address.fromBech32(taprootAddress);
-    return encodeSparkAddress({
-      identityPublicKey: Buffer.concat([Buffer.from([0x02]), outputPublicKey]).toString('hex'),
-      network: networkByType[networkType],
-    })
   }
 
   throw new ValidationError('Invalid taproot address');
