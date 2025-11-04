2c0ccf949797a959b3fd788912ea2a58625e1eb0
Matthew Rheaume <mhr@lightspark.com>
2025-11-03 23:27:43 -0500

    Sync from private repository
    GitOrigin-RevId: 1694843a53419f0ef9e6798a51d424a573a4cb21

--- spark/so/tree/tree.go
+++ spark/so/tree/tree.go
@@ -5,60 +5,12 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
-	"github.com/lightsparkdev/spark/common/keys"
 	"github.com/lightsparkdev/spark/common/logging"
-	pb "github.com/lightsparkdev/spark/proto/spark_tree"
 	"github.com/lightsparkdev/spark/so/ent"
 	st "github.com/lightsparkdev/spark/so/ent/schema/schematype"
 	"github.com/lightsparkdev/spark/so/ent/transferleaf"
-	"github.com/lightsparkdev/spark/so/ent/tree"
 	"github.com/lightsparkdev/spark/so/ent/treenode"
 )
-
-// DenominationMaxPow is the maximum power of 2 for leaf denominations.
-const DenominationMaxPow = 30
-
-// DenominationMax is the maximum allowed denomination value for a leaf, calculated as 2^DenominationMaxPow.
-const DenominationMax = uint64(1) << DenominationMaxPow
-
-// GetLeafDenominationCounts returns the counts of each leaf denomination for a given owner.
-func GetLeafDenominationCounts(ctx context.Context, req *pb.GetLeafDenominationCountsRequest) (*pb.GetLeafDenominationCountsResponse, error) {
-	logger := logging.GetLoggerFromContext(ctx)
-
-	network := st.Network(req.Network)
-	err := network.UnmarshalProto(req.Network)
-	if err != nil {
-		return nil, err
-	}
-
-	db, err := ent.GetDbFromContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-	ownerIdentityPubKey, err := keys.ParsePublicKey(req.OwnerIdentityPublicKey)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse owner identity public key: %w", err)
-	}
-	leaves, err := db.TreeNode.Query().
-		Where(treenode.OwnerIdentityPubkey(ownerIdentityPubKey)).
-		Where(treenode.StatusEQ(st.TreeNodeStatusAvailable)).
-		Where(treenode.HasTreeWith(tree.NetworkEQ(network))).
-		All(ctx)
-	if err != nil {
-		return nil, err
-	}
-	counts := make(map[uint64]uint64)
-	for _, leaf := range leaves {
-		// Leaves must be a power of 2 and less than or equal to the maximum denomination.
-		if leaf.Value&(leaf.Value-1) != 0 || leaf.Value > DenominationMax || leaf.Value == 0 {
-			logger.Sugar().Infof("Invalid leaf denomination %d", leaf.Value)
-			continue
-		}
-		counts[leaf.Value]++
-	}
-	logger.Sugar().Infof("Leaf count (leaves: %d, public key: %x)", len(leaves), ownerIdentityPubKey)
-	return &pb.GetLeafDenominationCountsResponse{Counts: counts}, nil
-}
 
 // Marks exiting nodes and their children with a proper status and confirmation height in batch update query to the DB.
 // It takes a list of confirmed in a bitcoin block transaction id hashes and sends it to Postgres to update the tree nodes that have those txids.
