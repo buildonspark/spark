297fe0ee04c0ff3ed7a7d4f6d80e2b862ba55429
Linden <70739041+linden@users.noreply.github.com>
2025-11-21 17:23:47 -0800

    fix(`spark-sdk`): skip minikube dependent tests when unavailable

--- sdks/js/packages/spark-sdk/src/tests/integration/transfer.test.ts
+++ sdks/js/packages/spark-sdk/src/tests/integration/transfer.test.ts
@@ -20,8 +20,7 @@ import {
   SparkWalletTestingIntegrationWithStream,
 } from "../utils/spark-testing-wallet.js";
 import { BitcoinFaucet } from "../utils/test-faucet.js";
-
-const testLocalOnly = process.env.GITHUB_ACTIONS ? it.skip : it;
+import { testMinikubeAndLocalOnly } from "../utils/utils.js";
 
 describe.each(walletTypes)(
   "Transfer with name",
@@ -121,7 +120,7 @@ describe.each(walletTypes)(
       expect(balance.balance).toBe(1000n);
     }, 30000);
 
-    testLocalOnly(`${name} - test transfer with separate`, async () => {
+    testMinikubeAndLocalOnly(`${name} - test transfer with separate`, async () => {
       const faucet = BitcoinFaucet.getInstance();
 
       const options: ConfigOptions = {
@@ -252,7 +251,7 @@ describe.each(walletTypes)(
       );
     });
 
-    testLocalOnly(
+    testMinikubeAndLocalOnly(
       `${name} - test that when the receiver has tweaked the key on some SOs, we can still claim the transfer`,
       async () => {
         const faucet = BitcoinFaucet.getInstance();
