aabd58ae1e15bc784627c204905f34bc03e1b151
Brian Chiang <brianc@lightspark.com>
2025-12-15 12:50:56 -0800

    Sync from private repository
    GitOrigin-RevId: c704797c8b441905484a09dd42f5c708cb4f102b

--- spark/so/knobs/knobs.go
+++ spark/so/knobs/knobs.go
@@ -75,6 +75,7 @@ const (
 	KnobFinalizeCreatedSignedOutputsJustInTime  = "spark.so.tokens.finalize_created_signed_outputs_just_in_time"
 	KnobTokenTransactionV3Enabled               = "spark.so.tokens.token_transaction_v3_enabled"
 	KnobBackfillCreatedReversedTokenAmount      = "spark.so.tokens.backfill_created_reversed_token_amount.enabled"
+	KnobAllowExtraMetadataOnMainnet             = "spark.so.tokens.allow_extra_metadata_on_mainnet"
 
 	// Number of confirmations required before finalizing tree creation
 	KnobNumRequiredConfirmations = "spark.so.num_required_confirmations"
