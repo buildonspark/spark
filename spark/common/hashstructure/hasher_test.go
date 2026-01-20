32dd2b68ec23684e75919ab4a3680d7ff38fe44c
keenan-lightspark <keenan@lightspark.com>
2026-01-20 08:30:59 -0800

    Sync from private repository
    GitOrigin-RevId: 08635df1779c12615b85405610c29cc2a2dc7d61

--- spark/common/hashstructure/hasher_test.go
+++ spark/common/hashstructure/hasher_test.go
@@ -1,9 +1,11 @@
 package hashstructure
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHasher_DomainSeparation(t *testing.T) {
@@ -77,4 +79,48 @@ func TestHasher_Deterministic(t *testing.T) {
 	}
 
 	assert.Equal(t, hashes[0], hashes[1])
+}
+
+func TestHasher_TestVectors(t *testing.T) {
+	type testCase struct {
+		name     string
+		expected string // hex-encoded expected hash
+		actual   []byte
+	}
+
+	testCases := []testCase{
+		{
+			name:     "empty tag",
+			expected: "2dba5dbc339e7316aea2683faf839c1b7b1ee2313db792112588118df066aa35",
+			actual: NewHasher([]string{}).
+				Hash(),
+		},
+		{
+			name:     "empty data",
+			expected: "c67afb9eb635e689553aefb4366b06372478967e813c0261377067f38257d48f",
+			actual: NewHasher([]string{"test", "vector"}).
+				Hash(),
+		},
+		{
+			name:     "all data types",
+			expected: "7e6d5afa6426b20f2f08929260f3e37cf280497eae6d29c1af313db01a8996a8",
+			actual: NewHasher([]string{"test", "vector"}).
+				AddBytes([]byte{1, 2, 3}).
+				AddString("string").
+				AddUint(1).
+				AddUint8(8).
+				AddUint16(16).
+				AddUint32(32).
+				AddUint64(64).
+				Hash(),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			expectedBytes, err := hex.DecodeString(tc.expected)
+			require.NoError(t, err, "invalid expected hex")
+			assert.Equal(t, expectedBytes, tc.actual, "mismatch for test vector %q, got %s", tc.name, hex.EncodeToString(tc.actual))
+		})
+	}
 }
